690393b1c12b445885653f03e0be708cd7d71a1e
DJeison <diegovt.arbt@gmail.com>
2024-10-31 16:25:17 -0600

    use godotenv

--- Makefile
+++ Makefile
@@ -2,11 +2,8 @@ FRONTEND_DIR = ./frontend_react_native
 BACKEND_DIR  = ./backend_go
 
 
-build_backend:
-	@cd ${BACKEND_DIR} && go build -o ./bin/FoodBank
-
-run_backend: build_backend
-	@cd ${BACKEND_DIR}/bin && ./FoodBank
+run_backend: 
+	@cd ${BACKEND_DIR} && go run main.go
 
 test_backend:
 	@cd $(BACKEND_DIR) && go test -v ./...
--- backend_go/config/config.go
+++ backend_go/config/config.go
@@ -1,13 +1,27 @@
 package config
 
 import (
+	"log"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 var (
-	DatabaseURL = os.Getenv("DATABASE_URL")
-
-	GoogleClientID     = os.Getenv("GOOGLE_CLIENT_ID")
-	GoogleClientSecret = os.Getenv("GOOGLE_CLIENT_SECRET")
-	GoogleRedirectURL  = os.Getenv("GOOGLE_REDIRECT_URL")
+	DatabaseURL        string
+	GoogleClientID     string
+	GoogleClientSecret string
+	GoogleRedirectURL  string
 )
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	DatabaseURL = os.Getenv("DATABASE_URL")
+	GoogleClientID = os.Getenv("GOOGLE_CLIENT_ID")
+	GoogleClientSecret = os.Getenv("GOOGLE_CLIENT_SECRET")
+	GoogleRedirectURL = os.Getenv("GOOGLE_REDIRECT_URL")
+}
