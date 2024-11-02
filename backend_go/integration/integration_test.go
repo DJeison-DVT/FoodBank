19fe65776ed3f7b20bac0e0ee1d75b3a156931c1
Sign0ret <adolfo.h.signoret@gmail.com>
2024-11-01 18:17:47 -0600

    donations cruds
    db functions working, handler tests working, integration tests not passed

--- backend_go/integration/integration_test.go
+++ backend_go/integration/integration_test.go
@@ -8,6 +8,7 @@ import (
 	"net/http/httptest"
 	"path/filepath"
 	"testing"
+	"fmt"
 
 	"backend_go/config"
 	"backend_go/database"
@@ -42,8 +43,9 @@ func setupIntegrationTestDB() *gorm.DB {
 	return db
 }
 
-func TestIntegration_ValidDonation(t *testing.T) {
-	database.DB = setupIntegrationTestDB()
+func testValidDonation(t *testing.T, db *gorm.DB) models.Donation {
+	// Set the database for the current test
+	database.DB = db
 
 	// Creating a new donation request
 	donation := models.Donation{
@@ -83,39 +85,87 @@ func TestIntegration_ValidDonation(t *testing.T) {
 		t.Errorf("Expected donation with type 'food' and details 'Canned goods', got %v", savedDonation)
 	}
 
-	// Clean up
-	database.DB.Unscoped().Delete(&models.Donation{}, "id = ?", savedDonation.ID)
+	return savedDonation
 }
 
-func TestIntegration_InvalidDonation(t *testing.T) {
-	database.DB = setupIntegrationTestDB()
 
-	// Creating a new donation request with missing fields
-	donation := models.Donation{
-		Type:    "",
-		Details: "",
-	}
+func TestIntegration_Donations_Create(t *testing.T) {
+	t.Run("valid data", func(t *testing.T) {
+		db := setupIntegrationTestDB()
+		savedDonation := testValidDonation(t, db)
 
-	jsonData, err := json.Marshal(donation)
-	if err != nil {
-		t.Fatalf("Failed to marshal donation: %v", err)
-	}
+		// Clean up
+		defer db.Unscoped().Delete(&models.Donation{}, "id = ?", savedDonation.ID)
+	})
 
-	req, err := http.NewRequest("POST", "/donations", bytes.NewBuffer(jsonData))
-	if err != nil {
-		t.Fatalf("Failed to create request: %v", err)
-	}
+	t.Run("invalid data", func(t *testing.T) {
+		database.DB = setupIntegrationTestDB()
 
-	req.Header.Set("Content-Type", "application/json")
-	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(handlers.CreateDonation)
-	handler.ServeHTTP(rr, req)
+		// Creating a new donation request with missing fields
+		donation := models.Donation{
+			Type:    "",
+			Details: "",
+		}
 
-	// Check the status code is 500 Internal Server Error
-	if status := rr.Code; status != http.StatusInternalServerError {
-		t.Errorf("Handler returned wrong status code: got %v, want %v", status, http.StatusInternalServerError)
-	}
+		jsonData, err := json.Marshal(donation)
+		if err != nil {
+			t.Fatalf("Failed to marshal donation: %v", err)
+		}
 
-	// Clean up
-	database.DB.Exec("DELETE FROM donations")
+		req, err := http.NewRequest("POST", "/donations", bytes.NewBuffer(jsonData))
+		if err != nil {
+			t.Fatalf("Failed to create request: %v", err)
+		}
+
+		req.Header.Set("Content-Type", "application/json")
+		rr := httptest.NewRecorder()
+		handler := http.HandlerFunc(handlers.CreateDonation)
+		handler.ServeHTTP(rr, req)
+
+		// Check the status code is 500 Internal Server Error
+		if status := rr.Code; status != http.StatusInternalServerError {
+			t.Errorf("Handler returned wrong status code: got %v, want %v", status, http.StatusInternalServerError)
+		}
+
+		// Clean up
+		database.DB.Exec("DELETE FROM donations")
+	})
+
+}
+
+func TestIntegration_Donations_Read(t *testing.T) {
+
+	t.Run("valid data", func(t *testing.T) {
+		db := setupIntegrationTestDB()
+		savedDonation := testValidDonation(t, db)
+
+		url := fmt.Sprintf("/donations/%d", savedDonation.ID)
+		req, err := http.NewRequest("GET", url, nil)
+		if err != nil {
+			t.Fatalf("Failed to create request: %v", err)
+		}
+		rr := httptest.NewRecorder()
+		handler := http.HandlerFunc(handlers.ReadDonation)
+		handler.ServeHTTP(rr, req)
+
+		if status := rr.Code; status != http.StatusOK {
+			t.Errorf("Handler returned wrong status code: got %v, want %v", status, http.StatusOK)
+		}
+		var readDonation models.Donation
+		err = json.NewDecoder(rr.Body).Decode(&readDonation)
+		if err != nil {
+			t.Errorf("Failed to decode response body: %v", err)
+		}
+
+		if readDonation.Type != savedDonation.Type || readDonation.Details != savedDonation.Details {
+			t.Errorf(`Expected read and created donations to match,
+			got types: %v and %v; got details: %v and %v`, 
+				readDonation.Type, savedDonation.Type, 
+				readDonation.Details, savedDonation.Details)
+		}		
+
+		// Clean up
+		defer db.Unscoped().Delete(&models.Donation{}, "id = ?", savedDonation.ID)
+	})
+	
 }
